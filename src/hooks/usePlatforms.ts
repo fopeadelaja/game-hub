b32c2e8ca0c17854e1cedfc3557c16f3cb9b8737
Mofopefoluwa Adelaja <115238899+fopeadelaja@users.noreply.github.com>
2026-01-29 12:08:12 +0100

    Implement infinite queries

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,9 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
-import apiClient from "../services/api-client";
-import type { FetchResponse } from "../services/api-client";
 import platforms from "../data/platforms";
+import APIClient from "../services/api-client";
 
-
+const apiClient = new APIClient<Platform>('/platforms/lists/parents');
 export interface Platform {
     id: number;
     name: string;
@@ -12,9 +11,7 @@ export interface Platform {
 
 const usePlatforms = () => useQuery({
     queryKey: ['platforms'],
-    queryFn: () => apiClient
-    .get<FetchResponse<Platform>>('/platforms/lists/parents')
-    .then(res => res.data),
+    queryFn: apiClient.getAll,
     staleTime: 24*60*60*1000, // 24 hours
     initialData: {count: platforms.length, results: platforms}
    
