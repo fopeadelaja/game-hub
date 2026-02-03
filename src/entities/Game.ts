bceda42c1d4a27f0c826f93d392d7d69cb8537f1
Mofopefoluwa Adelaja <115238899+fopeadelaja@users.noreply.github.com>
2026-02-03 23:04:26 +0100

    Build game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,4 +1,6 @@
+import type { Genre } from "./Genre";
 import type { Platform } from "./Platform";
+import type { Publisher } from "./Publisher";
 
 
 
@@ -6,6 +8,8 @@ export interface Game {
   id: number;
   name: string;
   slug: string;
+  genres: Genre[];
+  publishers: Publisher[]
   description_raw: string;
   background_image: string;
   parent_platforms: { platform: Platform; }[];
--- src/pages/GamesDetailPage.tsx
+++ src/pages/GamesDetailPage.tsx
@@ -1,6 +1,8 @@
 import { Heading, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
+
 import ExpandableText from "../components/ExpandableText";
+import GameAttributes from "../components/GameAttributes";
 import useGame from "../hooks/useGame";
 
 const GamesDetailPage = () => {
@@ -14,6 +16,7 @@ const GamesDetailPage = () => {
     <>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttributes game={game} />
     </>
   );
 };
