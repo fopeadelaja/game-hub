1e9143e8e02e9610dd6d83e060482f437549ee5f
Mofopefoluwa Adelaja <115238899+fopeadelaja@users.noreply.github.com>
2026-02-04 00:37:01 +0100

    Build game screenshots

--- src/pages/GamesDetailPage.tsx
+++ src/pages/GamesDetailPage.tsx
@@ -5,6 +5,7 @@ import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
 import useGame from "../hooks/useGame";
 import GameTrailer from "../components/GameTrailer";
+import GameScreenshots from "../components/GameScreenshots";
 
 const GamesDetailPage = () => {
   const { slug } = useParams();
@@ -19,6 +20,7 @@ const GamesDetailPage = () => {
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAttributes game={game} />
       <GameTrailer gameId={game.id} />
+      <GameScreenshots gameId={game.id} />
     </>
   );
 };
