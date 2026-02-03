b0840080147b17f5b140c4d234be442a19486c0b
Mofopefoluwa Adelaja <115238899+fopeadelaja@users.noreply.github.com>
2026-02-03 19:06:54 +0100

    Fetch game details

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -8,10 +8,13 @@ import useGameQueryStore from "../store";
 export interface Game {
   id: number;
   name: string;
+  slug: string;
+  description_raw: string;
   background_image : string
   parent_platforms: {platform: Platform}[];
   metacritic: number;
   rating_top: number;
+  
 
 }
 const apiClient = new APIClient<Game>('/games' );
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -27,6 +27,13 @@ import axios, { type AxiosRequestConfig } from "axios";
         .get<FetchResponse<T>>(this.endpoint, config)
         .then(res=> res.data)
     }
+
+    get = (id: number | string) => {
+        return axiosInstance     
+        .get<T>(this.endpoint + '/' + id)
+        .then(res=> res.data);
+    }
+
  }
 
 export default APIClient
